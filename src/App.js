6a5bf41edfa961b1ec8df22dd0a96e7e56eb12be
Rui Guo <guojiarui@gmail.com>
2022-06-28 14:33:13 -0700

    Bookshelf and BestBook added.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,14 @@
 import React from 'react';
-import axios from 'axios';
-// import Bestbooks from './Bestbooks';
+// import axios from 'axios';
+import BestBooks from './BestBooks';
 import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
   Link,
 } from "react-router-dom";
 
-const SERVER = process.env.REACT_APP_SERVER;
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [],
-    }
-  }
-
-  getBooks = async () => {
-    console.log('I fired');
-    try {
-      let results = await axios.get(`${SERVER}/books`);
-      console.log(results.data);
-      this.setState({
-        books: results.data
-      })
-    } catch(error){
-      console.log('we have an error: ', error.response.data)
-    }
-  }
-
-  componentDidMount() {
-    this.fetchBooks();
-  }
-
-  async fetchBooks(location = null) {
-    let apiUrl = `${SERVER}/books`;
-
-    if (location) {
-      apiUrl += `?location=${location}`;
-    }
-
-    try {
-      const response = await axios.get(apiUrl);
-      this.setState({ books: response.data });
-
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  handleLocationSubmit = (event) => {
-    event.preventDefault();
-    const location = event.target.location.value;
-    console.log({ location });
-    this.fetchBooks(location);
-  }
 
   render() {
     return (
@@ -68,22 +19,12 @@ class App extends React.Component {
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
           </nav>
-          <Switch>
-            <Route exact path="/">
-
-              <div>
-                <Books books={this.state.books} />
-                <h2>Filter by location</h2>
-                <form onSubmit={this.handleLocationSubmit}>
-                  <input name="location" />
-                  <button>ok</button>
-                </form>
-              </div>
-            </Route>
-            <Route path="/about">
-              <h1>About Page Here</h1>
-            </Route>
-          </Switch>
+          <Route
+            exact path="/" element={<BestBooks />}>
+          </Route>
+          <Route path="/about">
+            <h1>About Page Here</h1>
+          </Route>
         </Router>
       </>
     )
---
+++ src/BestBooks.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+import Bookshelf from './Bookshelf';
+
+class BestBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
+
+  getBooks = async () => {
+    console.log('I fired');
+    try {
+      let results = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
+      console.log(results.data);
+      this.setState({
+        books: results.data,
+        description: results.description,
+        status: true
+      })
+    } catch (error) {
+      console.log('we have an error: ', error.response.data)
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Book Shelf</h2>
+        {this.state.books.length ? (
+          <Bookshelf
+            bookData={this.state.books}
+          />
+        ) : (
+          <h3>No books</h3>
+        )}
+      </>
+    )
+  }
+}
+
+
+
+export default BestBooks;
