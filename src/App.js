b0326e7bd7170dd282af34c66a2c1d90f8e1b90b
Rui Guo <guojiarui@gmail.com>
2022-06-27 22:34:48 -0700

    app.js updated.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import Cats from './Bestbooks';
+// import Bestbooks from './Bestbooks';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -11,22 +11,21 @@ import {
 
 const SERVER = process.env.REACT_APP_SERVER;
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cats: [],
+      books: [],
     }
   }
 
-  getCats = async () => {
+  getBooks = async () => {
     console.log('I fired');
     try {
-      let results = await axios.get(`${SERVER}/cats`);
+      let results = await axios.get(`${SERVER}/books`);
       console.log(results.data);
       this.setState({
-        cats: results.data
+        books: results.data
       })
     } catch(error){
       console.log('we have an error: ', error.response.data)
@@ -34,38 +33,38 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.fetchCats();
+    this.fetchBooks();
   }
 
-  // async fetchCats(location = null) {
-  //   let apiUrl = `${SERVER}/cats`;
+  async fetchBooks(location = null) {
+    let apiUrl = `${SERVER}/books`;
 
-  //   if (location) {
-  //     apiUrl += `?location=${location}`;
-  //   }
+    if (location) {
+      apiUrl += `?location=${location}`;
+    }
 
-  //   try {
-  //     const response = await axios.get(apiUrl);
-  //     this.setState({ cats: response.data });
+    try {
+      const response = await axios.get(apiUrl);
+      this.setState({ books: response.data });
 
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  // handleLocationSubmit = (event) => {
-  //   event.preventDefault();
-  //   const location = event.target.location.value;
-  //   console.log({ location });
-  //   this.fetchCats(location);
-  // }
+  handleLocationSubmit = (event) => {
+    event.preventDefault();
+    const location = event.target.location.value;
+    console.log({ location });
+    this.fetchBooks(location);
+  }
 
   render() {
     return (
       <>
         <Router>
           <nav>
-            <h1>World of Cats</h1>
+            <h1>World of Books</h1>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
           </nav>
@@ -73,14 +72,13 @@ class App extends React.Component {
             <Route exact path="/">
 
               <div>
-                <Cats cats={this.state.cats} />
+                <Books books={this.state.books} />
                 <h2>Filter by location</h2>
                 <form onSubmit={this.handleLocationSubmit}>
                   <input name="location" />
                   <button>ok</button>
                 </form>
               </div>
-
             </Route>
             <Route path="/about">
               <h1>About Page Here</h1>
