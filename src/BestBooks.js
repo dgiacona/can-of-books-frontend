ab0dfc9579be9e57b6d6bfffb51c5f50c6665748
Rui Guo <guojiarui@gmail.com>
2022-06-28 22:08:45 -0700

    delete function added.

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import Bookshelf from './Bookshelf';
 import { Button, Container, Form } from 'react-bootstrap';
 
+const SERVER = process.env.REACT_APP_SERVER;
+const API_URL = `${SERVER}/books`;
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
@@ -31,7 +33,7 @@ class BestBooks extends React.Component {
     let config = {
       method: 'post',
       url: `${process.env.REACT_APP_SERVER}/books`,
-      data: book  
+      data: book
     }
     try {
       let results = await axios(config);
@@ -55,7 +57,7 @@ class BestBooks extends React.Component {
       this.setState({
         books: updatedBooks
       });
-    } catch(error) {
+    } catch (error) {
       console.log('we have an error: ', error.response.data);
     }
   }
@@ -75,27 +77,41 @@ class BestBooks extends React.Component {
     this.postBooks(newBook);
   }
 
+
+  handleDelete = async (bookToDelete) => {
+    const url = `${API_URL}/${bookToDelete._id}`;
+
+    try {
+      const response = await axios.delete(url);
+      console.log(response.data);
+      const filteredBooks = this.state.books.filter(book => book._id !== bookToDelete._id);
+      this.setState({ books: filteredBooks });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     return (
       <>
         <h2>Book Shelf</h2>
 
         <Container className="mt-5">
-            <Form onSubmit={this.handleBookSubmit}>
-              <Form.Group controlId="title">
-                <Form.Label>Title</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Form.Group controlId="description">
-                <Form.Label>Description</Form.Label>
-                <Form.Control type="text" />
-              </Form.Group>
-              <Form.Group controlId="status">
-                <Form.Check type="checkbox" label="completed" />
-              </Form.Group>
-              <Button type="submit">Add a Book</Button>
-            </Form>
-          </Container>
+          <Form onSubmit={this.handleBookSubmit}>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group controlId="description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group controlId="status">
+              <Form.Check type="checkbox" label="completed" />
+            </Form.Group>
+            <Button type="submit">Add Book</Button>
+          </Form>
+        </Container>
 
         {this.state.books.length ? (
           <Bookshelf
@@ -104,6 +120,19 @@ class BestBooks extends React.Component {
         ) : (
           <h3>No books</h3>
         )}
+
+        <Container className="mt-5">
+          <Form onSubmit={this.handleDelete}>
+            <Form.Group controlId="delete">
+              <Form.Label>would you like to delete this book?</Form.Label>
+          {/* // <Bookshelf books={this.state.books} onDelete={this.handleDelete} /> */}
+            </Form.Group>
+            <Button type="submit" variant="danger">Delete Book</Button>
+          </Form>
+        </Container>
+
+
+        {/* <Button type="button" variant="danger">Delete Book</Button> */} 
       </>
     )
   }
